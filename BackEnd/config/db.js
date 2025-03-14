eb7316dfc00c4199100ef08969580b2401aa9088
Hemanth das <hemanthchoudary4@gmail.com>
2025-03-14 23:21:11 +0530

    Update: Auth Settup Completed

---
+++ BackEnd/config/db.js
@@ -0,0 +1,23 @@
+const Sequlize = require("sequelize");
+require("dotenv").config();
+
+const db = new Sequlize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.error("Database connection failed", error);
+  }
+})();
+module.exports = db;
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const app = express();
+const { sequelize } = require("./models");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
 
+const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use("/auth", authRoutes);
 const PORT = process.env.PORT || 3000;
-
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Database synced");
+  })
+  .catch((error) => {
+    console.error("Database sync failed", error);
+  });
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
